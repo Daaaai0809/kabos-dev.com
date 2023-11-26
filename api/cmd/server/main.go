862a46bc075c77f764238971b8a9e3db8a437057
Daaaai0809 <dai.tsuruga@80and.co>
2023-11-27 01:46:32 +0900

    feat: rename gateway to auth

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/Daaaai0809/kabos-dev.com/adapter/gateway"
+	"github.com/Daaaai0809/kabos-dev.com/adapter/auth"
 	"github.com/Daaaai0809/kabos-dev.com/adapter/handler"
 	"github.com/Daaaai0809/kabos-dev.com/usecase"
 	"github.com/labstack/echo/v4"
@@ -11,7 +11,7 @@ func main() {
 	e := echo.New()
 
 	authInteractor := usecase.NewAuthInteractor()
-	authGateway := gateway.NewAuthGateway(authInteractor)
+	authGateway := auth.NewAuthGateway(authInteractor)
 
 	apiGroup := e.Group("/api")
 	apiGroup.GET("/", authGateway.Authorize(func(c echo.Context) error {
