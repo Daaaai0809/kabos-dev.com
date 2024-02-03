997b78bc68ee4ca5b853fb533c07690fb4ec23c1
Daaaai0809 <dai.tsuruga@80and.co>
2024-02-03 15:06:38 +0900

    chore: title変更

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="ja">
+    <Html lang="ja" title="Kabos's Portfolio">
       <Head />
       <body>
         <Main />
