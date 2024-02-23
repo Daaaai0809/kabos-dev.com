7c82597046ac48d606ecaa84d9521b9f5dce3e9e
Daaaai0809 <dai.tsuruga@80and.co>
2024-02-23 20:30:37 +0900

    reafctor: animation追加(product detail)

--- client/src/components/pages/products/Detail/index.css.ts
+++ client/src/components/pages/products/Detail/index.css.ts
@@ -1,4 +1,15 @@
-import { style } from '@vanilla-extract/css';
+import { keyframes, style } from '@vanilla-extract/css';
+
+const productDetailAnimation = keyframes({
+    '0%': {
+        opacity: 0,
+        transform: 'translateY(-10px)',
+    },
+    '100%': {
+        opacity: 1,
+        transform: 'translateY(0)',
+    },
+});
 
 export const productDetailPageStyle = {
     div: style({
@@ -8,5 +19,7 @@ export const productDetailPageStyle = {
         justifyContent: 'center',
         width: '100%',
         maxWidth: '1440px',
+        animationName: productDetailAnimation,
+        animationDuration: '0.8s',
     }),
 }
