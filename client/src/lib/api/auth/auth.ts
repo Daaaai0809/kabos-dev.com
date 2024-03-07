4ce4b2d0a5ceb9b0a8b998e200026604489b1e1a
Daaaai0809 <dai.tsuruga@80and.co>
2024-03-07 16:37:03 +0900

    feat: lib/Auth追加

---
+++ client/src/lib/api/auth/auth.ts
@@ -0,0 +1,29 @@
+import { LoginRequest } from "./type";
+
+export const login = async (req: LoginRequest) => {
+    const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req),
+    });
+
+    return res;
+};
+
+export const logout = async () => {
+    const res = await fetch('/api/auth/logout', {
+        method: 'POST',
+    });
+
+    return res;
+};
+
+export const getMe = async () => {
+    const res = await fetch('/api/auth/me', {
+        method: 'GET',
+    });
+
+    return res;
+};
