22776221ecc16af6bdc584cd4a86125800c45c0c
Daaaai0809 <dai.tsuruga@80and.co>
2024-04-20 15:37:06 +0900

    feat: modify 中間テーブルをdomainから削除

--- api/domain/repository/blog_repository.go
+++ api/domain/repository/blog_repository.go
@@ -10,7 +10,7 @@ type IBlogRepository interface {
 	GetAll(ctx context.Context) ([]*models.Blog, error)
 	GetSearchedBlog(ctx context.Context, searchWord string) ([]*models.Blog, error)
 	GetByID(ctx context.Context, id int) (*models.Blog, error)
-	Create(ctx context.Context, blog *models.Blog) (int, error)
-	Update(ctx context.Context, blog *models.Blog) error
+	Create(ctx context.Context, blog *models.Blog, tagIds []int) (int, error)
+	Update(ctx context.Context, blog *models.Blog, tagIds []int) error
 	Delete(ctx context.Context, id int) error
 }
--- api/models/blog_tags.go
+++ api/models/blog_tags.go
@@ -1,18 +1,10 @@
 package models
 
-import (
-	"github.com/Daaaai0809/kabos-dev.com/domain/entity"
-)
-
 type BlogTags struct {
 	BlogID int   `bun:"blog_id,pk"`
 	Blog   *Blog `bun:"rel:belongs-to,join:blog_id=id"`
 	TagID  int   `bun:"tag_id,pk"`
 	Tag    *Tag  `bun:"rel:belongs-to,join:tag_id=id"`
-}
-
-func (bt *BlogTags) ToBlogTagsEntity() *entity.BlogTags {
-	return entity.NewBlogTagsEntity(bt.BlogID, bt.TagID)
 }
 
 func NewBlogTagsModel(blogID, tagID int) *BlogTags {
