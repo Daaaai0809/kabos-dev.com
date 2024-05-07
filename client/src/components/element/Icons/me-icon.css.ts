f460d78f3376c60ac365416260a984569fc7fa63
Daaaai0809 <dai.tsuruga@80and.co>
2024-05-07 19:42:55 +0900

    feat: implement icons

---
+++ client/src/components/element/Icons/me-icon.css.ts
@@ -0,0 +1,14 @@
+import { style } from "@vanilla-extract/css";
+
+export const meIconStyles = {
+  header: style({
+    width: "3rem",
+    height: "3rem",
+    borderRadius: "50%",
+  }),
+  index: style({
+    width: "10rem",
+    height: "10rem",
+    borderRadius: "50%",
+  }),
+};
---
+++ client/src/components/element/Icons/zenn-icon.tsx
@@ -0,0 +1,13 @@
+import { ZennIconStyle } from "./zenn-icon.css";
+
+export const ZennIcon: React.FC = () => {
+  return (
+    <>
+      <img
+        src="/icons/links/Zenn-Black.svg"
+        alt="Zenn"
+        className={ZennIconStyle}
+      />
+    </>
+  );
+};
