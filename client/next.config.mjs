85120106431edd0fa10676c98da37fdf8ac5e24b
Daaaai0809 <dai.tsuruga@80and.co>
2024-05-09 02:21:29 +0900

    feat: add mdx setting, radix, react-feather

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,10 +1,16 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import WithMDX from '@next/mdx';
+
+const withMDX = WithMDX({
+  extension: /\.mdx?$/,
+});
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['tsx', 'ts', 'js', 'jsx', 'mdx'],
 };
 
-export default withVanillaExtract(nextConfig);
+export default withMDX(withVanillaExtract(nextConfig));
