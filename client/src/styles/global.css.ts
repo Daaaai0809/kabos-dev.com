6e53357fa5200e1778f381562fe8e57e2c790777
Daaaai0809 <dai.tsuruga@80and.co>
2024-05-08 17:47:16 +0900

    refacotr: 不要なファイル削除, global.css修正

--- client/src/styles/global.css.ts
+++ client/src/styles/global.css.ts
@@ -5,9 +5,11 @@ import { vars } from "./theme.css";
 globalStyle("body", {
   backgroundColor: vars.semantic.background.primary,
   fontFamily: '"Noto Sans JP", sans-serif',
-  margin: 0,
+  margin: "1rem 0.5rem",
   padding: 0,
   color: vars.semantic.text.primary,
+  display: "flex",
+  justifyContent: "center",
 
   minHeight: "100dvh",
   height: "100%",
