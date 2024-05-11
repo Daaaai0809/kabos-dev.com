b5d7541b350f102675183f6b430b87d3213c76f0
Daaaai0809 <dai.tsuruga@80and.co>
2024-05-11 14:52:12 +0900

    feat: add me-icons' shadow

--- client/src/components/element/Icons/me-icon.css.ts
+++ client/src/components/element/Icons/me-icon.css.ts
@@ -1,3 +1,4 @@
+import { vars } from "@/styles/theme.css";
 import { style } from "@vanilla-extract/css";
 
 export const meIconStyles = {
@@ -5,10 +6,12 @@ export const meIconStyles = {
     width: "3rem",
     height: "3rem",
     borderRadius: "50%",
+    boxShadow: `0 0 5px 1px ${vars.color.gray[5]}`,
   }),
   index: style({
     width: "10rem",
     height: "10rem",
     borderRadius: "50%",
+    boxShadow: `0 0 20px 3px ${vars.color.gray[5]}`,
   }),
 };
