58e1643a7939060c0fb649cbcecc40a077c243e9
Daaaai0809 <dai.tsuruga@80and.co>
2024-05-17 03:05:39 +0900

    feat: implement additional icons

---
+++ client/src/components/element/Icons/zenn-link.tsx
@@ -0,0 +1,13 @@
+import { ZennLinkStyle } from "./zenn-link.css";
+
+export const ZennLink: React.FC = () => {
+  return (
+    <>
+      <img
+        src="/icons/links/Zenn-Link.svg"
+        alt="Zenn"
+        className={ZennLinkStyle}
+      />
+    </>
+  );
+};
