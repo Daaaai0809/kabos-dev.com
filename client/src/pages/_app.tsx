03cb03e184a38d816ea20145e27464998d241169
Daaaai0809 <dai.tsuruga@80and.co>
2024-05-18 19:47:56 +0900

    chore: change layouts' dir

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useMemo } from "react";
-import { Layout } from "@/components/layout";
+import { Layout } from "@/components/layout/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = useMemo(
