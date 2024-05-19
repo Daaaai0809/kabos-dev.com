39a877b4264bdbcab35463802cb9e0664b78e413
Daaaai0809 <dai.tsuruga@80and.co>
2024-05-20 00:09:06 +0900

    refactor: header style

--- client/src/components/layout/header/header.css.ts
+++ client/src/components/layout/header/header.css.ts
@@ -7,14 +7,20 @@ export const headerStyles = {
     zIndex: 3,
     width: "100%",
     height: "auto",
-    maxHeight: "48px",
+    maxHeight: "64px",
     maxWidth: "688px",
+    boxSizing: "border-box",
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
     backgroundColor: "rgba(229, 241, 240, 80%)",
     backdropFilter: "blur(3px)",
-    padding: "0.5rem 0 0.5rem 0",
+    padding: "0.5rem 0",
+    "@media": {
+      "screen and (max-width: 768px)": {
+        padding: "0.5rem 1rem",
+      },
+    },
   }),
   imgLink: style({
     width: "48px",
@@ -28,7 +34,6 @@ export const headerStyles = {
     "@media": {
       "screen and (max-width: 768px)": {
         gap: vars.spacing[3],
-        padding: "0 2rem 0 0",
       },
     },
   }),
