a000547f1741b0db9abd90634635c36da6bff11d
Daaaai0809 <dai.tsuruga@80and.co>
2024-12-09 19:41:42 +0900

    feat : admin側の環境構築

---
+++ admin/src/routes.tsx
@@ -0,0 +1,16 @@
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+} from 'react-router-dom';
+import App from './App';
+
+const AppRoutes = () => (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+    </BrowserRouter>
+)
+
+export default AppRoutes;
