3ff86bb2a6fef650090d64346fe52a3b17d669e9
Daaaai0809 <130345626+Daaaai0809@users.noreply.github.com>
2025-09-18 15:42:40 +0900

    career更新 (#94)
    * feat: remake client dir
    
    * feat: add biome
    
    * feat: add workflows
    
    * feat: modify 中間テーブルをdomainから削除
    
    * format: go fmt
    
    * feat: modify blog table column
    
    * feat: modify Blog Presenter
    
    * format: go fmt
    
    * feat: setting storybook
    
    * feat: tsconfigの設定を反映させるplugin追加
    
    * feat: setting vanilla-extract
    
    * feat: 全体で使うコンポーネントの整備 (#72)
    
    * feat: add icon images
    
    * feat: add global, contracts, theme
    
    * feat: storyBookでvannila-extractを使えるように
    
    * feat: implement icons
    
    * feat: implement headers
    
    * refactor: 不要な部分の削除, ページにCSS適用
    
    * feat: implement index page (#73)
    
    * feat: add career & skills constants
    
    * feat: add story book config
    
    * refacotr: 不要なファイル削除, global.css修正
    
    * feat: add mdx setting, radix, react-feather
    
    * feat: add about-me mdx
    
    * feat: add Header, change title
    
    * feat: implement about page elements
    
    * feat: implement footer, layout
    
    * feat: add me-icons' shadow
    
    * feat: implement about-me-index
    
    * feat: implement index page + スタイルの細かい修正
    
    * fix: footer, headerのmemo内に名前付け, _documentエラー修正
    
    * Feat/#65 impl blog list page (#75)
    
    * refacotr: modify pathnames
    
    * feat: add Emoji Column for blogs table
    
    * feat: implement feature repositories
    
    * refactor: modify global style
    
    * feat: implement additional icons
    
    * feat: implement blog list ui
    
    * chore: update packages, modify configs
    
    * Feat/#66 blog detail (#77)
    
    * feat: implement inside blogs
    
    * feat: implement remark/rehype
    
    * feat: implement tag-list ui
    
    * feat: implement md-block, content-area
    
    * chore: change layouts' dir
    
    * feat: implement blog-detail page
    
    * feat: implement r2 bucket operation (#80)
    
    * Feat/#67 add works page (#81)
    
    * chore: modify product seeder
    
    * refctor: modify r2 operation
    
    * feat: implement product operation
    
    * feat: implement product list ui
    
    * refactor: modify global style, header, blog-list-page
    
    * refactor: header style
    
    * Feat/#68 add product detail (#82)
    
    * refactor: modify md-block style
    
    * feat: implement product detail page ui
    
    * Feat/#78 modify skill description (#83)
    
    * feat: change contact href
    
    * refactor: skill description, styles
    
    * feat: add migration file add column content to blogs
    
    * feat: add career of line-yahoo (#85)
    
    * chore : 自己紹介文, Careerの更新 (#89)
    
    * 年度計算のロジック修正, esmoduleのバージョン上げ (#91)
    
    * env :  ESNextにバージョン上げ
    
    * fix : getMonthに+1するように修正
    
    * Add new career entries for internships (#93)
    
    * Add new career entries for internships
    
    * fix: modify dependencies

--- client/src/constants/careers.ts
+++ client/src/constants/careers.ts
@@ -18,6 +18,7 @@ const careers: Careers = [
   {
     name: "株式会社80&Company",
     startedAt: "2023/04",
+    endedAt: "2025/08",
     icon: "/icons/careers/80&co.svg",
     description:
       "2023年4月より長期インターンとして働いています。\nこれまでドレスレンタルのECサイトや、オンラインアートスクール、工芸品サブスクサービスなどの開発プロジェクトに携わってきました。",
@@ -31,9 +32,21 @@ const careers: Careers = [
   {
     name: "LINEヤフー株式会社",
     startedAt: "2024/08",
+    endedAt: "2024/08",
     icon: "/icons/careers/work.svg",
     description:
       "2024年8月中旬よりLINEヤフー株式会社にてサマーインターンシップに参加しました。 \nインターンではヤフーアプリにおける広告配信基盤のKubernetesアーキテクチャの改修タスクを主に担当し、Daemon SetやHome Pathを利用した静的ファイルの配置や取得用Cronスクリプトの改修によって、静的ファイルの占有領域を1/10以下にすることに成功しました。\nまた、既存のShell ScriptプログラムのGolangへの移行タスクも担当しました。",
+  },
+  {
+    name: "合同会社Unics",
+    startedAt: "2025/06",
+    icon: "/icons/careers/work.svg",
+  },
+  {
+    name: "LINEヤフー株式会社",
+    startedAt: "2025/08",
+    endedAt: "2025/09",
+    icon: "/icons/careers/work.svg",
   },
 ];
 
